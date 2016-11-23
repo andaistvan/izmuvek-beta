17256973e422ff9d46a09125878962d46e6ca531
andaistvan <andaistvan@gmail.com>
2016-11-23 13:10:19 +0100

    myaccount basic format

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -279,6 +279,17 @@ function sww_edit_order_item_name($name)
 add_filter('woocommerce_order_item_name', 'sww_edit_order_item_name');
 
 // ***************************************************************
+// remove password strength'o meter
+// ***************************************************************
+function wc_ninja_remove_password_strength()
+{
+    if (wp_script_is('wc-password-strength-meter', 'enqueued')) {
+        wp_dequeue_script('wc-password-strength-meter');
+    }
+}
+add_action('wp_print_scripts', 'wc_ninja_remove_password_strength', 100);
+
+// ***************************************************************
 // my account custom tab
 // ***************************************************************
 /**
